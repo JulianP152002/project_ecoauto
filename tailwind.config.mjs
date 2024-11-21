62ba733203735ce3b02966d2d272cf9e6accbbf2
JulianP152002 <parracardenas17@gmail.com>
2024-11-20 21:33:08 -0500

    ultimates details

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,30 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-		},
-		screens: {
-			'2xl': {'max': '1535px'},
-			// => @media (max-width: 1535px) { ... }
-	  
-			'xl': {'max': '1279px'},
-			// => @media (max-width: 1279px) { ... }
-	  
-			'lg': {'max': '1023px'},
-			// => @media (max-width: 1023px) { ... }
-			'mm': {'max': '850px'},
-			
-			'md': {'max': '767px'},
-			// => @media (max-width: 767px) { ... }
-	  
-			'sm': {'max': '639px'},
-			// => @media (max-width: 639px) { ... }
-			'xx': {'max': '530px'},
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+    screens: {
+      
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
 
-			'xm': {'max': '440px' }
-		  }
-	},
-	plugins: [],
-}
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+      mm: { max: "850px" },
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+      xx: { max: "530px" },
+
+      xm: { max: "440px" },
+    },
+  },
+  plugins: [],
+};
